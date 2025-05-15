7eb7aedf964f69e921d06d4b159213b3c491c479
Clement Cravero <clement@cravero.fr>
2025-05-16 00:05:04 +0200

    nginx

--- copyFile.ps1
+++ copyFile.ps1
@@ -1,8 +1,10 @@
 # Copier le contenu du dossier ../front/ vers ./front
-Copy-Item -Path "..\front\*" -Destination ".\front" -Recurse -Force
+# Copy-Item -Path "..\front\*" -Destination ".\front" -Recurse -Force
 
 # Copier les fichiers .jar du dossier Service1 vers ./service1
-Copy-Item -Path "..\Service1\Service1\demo\target\*.jar" -Destination ".\service1" -Force
+Copy-Item -Path "..\Service1\Service1\demo\target\*.jar" -Destination ".\service1\service1.jar" -Force
 
 # Copier les fichiers .jar du dossier Service2 vers ./service2
-Copy-Item -Path "..\Service2\Service2\demo\target\*.jar" -Destination ".\service2" -Force
+Copy-Item -Path "..\Service2\Service2\demo\target\*.jar" -Destination ".\service2\service2.jar" -Force
+# Copier les fichiers .jar du dossier Service3 vers ./middleware
+Copy-Item -Path "..\Middleware\demo\demo\target\*.jar" -Destination ".\middleware\middleware.jar" -Force
